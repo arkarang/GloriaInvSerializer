bc3ade2003947687ba6dcf1893533358b13ce8e5
Arkarang <pommek@naver.com>
2023-10-03 18:46:23 +0900

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.name = "GloriaInvSerializer"
+include("nms-1182")
+
+pluginManagement {
+
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+include("common")
+include("nms-1194")
